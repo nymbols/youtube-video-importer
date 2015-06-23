65c728158ba52ff736e1f5c9930ac72490d808e0
youtubeimporter <youtubeimporter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-23 10:46:01 +0000

    Delete Old Files
    git-svn-id: https://plugins.svn.wordpress.org/youtube-video-importer/trunk@1186107 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- assets/back-end/js/tinymce/langs/en.js
+++
@@ -1,3 +0,0 @@
-tinyMCE.addI18n("en.yytt_shortcode",{
-	title : "Embed video"
-});
--- views/help/automatic-import-overview.html.php
+++
@@ -1,5 +0,0 @@
-<p>
-	<?php _e('This screen provides access to all your automatic playlists.', 'yti_video');?><br />
-	<?php _e('To create a new playlist just click Add New next to the page title.', 'yti_video');?><br />
-	<?php _e('To change import frequency please visit Settings page from plugin menu.', 'yti_video');?>
-</p>
