d3539d6b40413d41b1407ead60cef9ffeb9da7ba
youtubeimporter <youtubeimporter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-27 10:25:00 +0000

    Update tabs
    git-svn-id: https://plugins.svn.wordpress.org/youtube-video-importer/trunk@1188517 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- assets/adminpenal/js/tabs.js
+++ assets/adminpenal/js/tabs.js
@@ -1,67 +1,22 @@
+
 /**
- * 
+ * accordion
  */
 ;(function($){
 	
 	$(document).ready(function(){
 		// tabs
-		if( typeof(Storage) !== "undefined" ){
-			var data = {
-				active : sessionStorage['yvi_tab_active'],
-				activate : function(event, ui){
-					$(ui.newTab).find('i')
-						.removeClass('dashicons-arrow-right')	
-						.addClass('dashicons-arrow-down');
-					
-					$(ui.oldTab).find('i')
-						.addClass('dashicons-arrow-right')	
-						.removeClass('dashicons-arrow-down');
-					
-					sessionStorage['yvi_tab_active'] = ui.newTab.index();
-				},
-				create: function(event, ui){
-					$(ui.tab).find('i')
-						.removeClass('dashicons-arrow-right')	
-						.addClass('dashicons-arrow-down');
-				}
-			};
-		}else{
-			var data = {};
-		};
-		
-		var h = $(location).attr('hash');
-		if( '' != h ){
-			var item = $( '.yvi-tab-labels a[href=' + h + ']' );
-			if( item.length > 0 ){
-				var items = $( '.yvi-tab-labels a' ),
-					index = $.inArray( item[0], $.makeArray( items ) );
-				if( -1 != index ){
-					data.active = index;
-				}				
-			} 
+		$("div.tab").click(function(){
+		if($(this).hasClass("active")!==true)
+		{
+			$('.tab').removeClass("active");
+			$(this).addClass("active");
+			id=$(this).attr("id");
+			$('.content').hide();
+			$("."+id).slideDown();
 		}
-		
-		$( "#yvi_tabs" ).tabs( data );
-		// end tabs
-		
-		var checkbox = $('.toggler').find('input[type=checkbox]');
-		$.each(checkbox, function(i, ch){
-			var tbl = $(this).parents('table'),
-				tr = $(tbl).find('tr.toggle');
-			
-			if( !$(this).is(':checked') ){
-				$(tr).hide();
-			}	
-			
-			$(this).click(function(){
-				if( $(ch).is(':checked') ){
-					$(tr).show(400);
-				}else{
-					$(tr).hide(200);
-				}
-			});
-			
 		});
+
 		
 	});
 	
